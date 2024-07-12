f6bb8eef227c4dd212d29d23ef962b4eb13bfd9a
WAHU0508 <sharongikenye@gmail.com>
2024-07-12 10:31:18 +0300

    Edit likes

--- src/index.js
+++ src/index.js
@@ -37,12 +37,36 @@ document.addEventListener("DOMContentLoaded", () => {
 function toyCard(toy) {
   const cardDiv = document.createElement('div');
   cardDiv.className = 'card';
-  cardDiv.innerHTML = `
-    <h2>${toy.name}</h2>
-    <img class="toy-avatar" src="${toy.image}" alt="${toy.name}">
-    <p>${toy.likes} Likes</p>
-    <button class="like-btn" data-id="${toy.id}">Like ❤️</button>
-  `;
+  const heading = document.createElement('h2');
+  heading.textContent = toy.name
+  const image = document.createElement('img');
+  image.src = toy.image
+  image.alt = toy.name
+  image.className = "toy-avatar"
+  const p = document.createElement('p');
+  p.textContent = toy.likes
+  const button = document.createElement('button')
+  button.className = "like-btn"
+  button.textContent = 'Like ❤️'
+  button.addEventListener('click', () => {
+    fetch(`http://localhost:3000/toys/${toy.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({ likes: toy.likes + 1 })
+    })
+    .then(res => res.json())
+    .then(toy => {
+      p.textContent = toy.likes;
+    })
+    .catch(error => console.error(`Error: ${error}`));
+  })
+  cardDiv.appendChild(heading)
+  cardDiv.appendChild(image)
+  cardDiv.appendChild(p)
+  cardDiv.appendChild(button)
   toyCollection.appendChild(cardDiv);
 }
 
@@ -72,21 +96,6 @@ function postingToys(name, image) {
   .then(toy => toyCard(toy))
   .catch(error => console.error(`Error: ${error}`));
 }
-let mewLikes = id.likes
-function likeToy(id) {
-  fetch(`http://localhost:3000/toys/${id}`, {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    },
-    body: JSON.stringify({ likes: newLikes++})
-  })
-  .then(res => res.json());
-}
 
-function updateToyLikes(id, likes) {
-  console.log('Updating likes for toy:', id, 'to', likes);
-  const toyCard = document.querySelector(`button[data-id="${id}"]`).closest('.card');
-  toyCard.querySelector('p').textContent = `${likes} Likes`;
-}
+
+
